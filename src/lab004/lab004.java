4b821b6a111d43830d223980fe632c33f28d56c5
DetsuyaOoO <Sslermontov@gmail.com>
2022-08-21 14:32:33 +0300

    Initial commit

---
+++ src/lab004/lab004.java
@@ -0,0 +1,27 @@
+package lab004;
+import java.lang.String;
+import java.util.Scanner;
+
+
+
+ class Main {
+
+    public static void main(String[] args) {
+        int result;
+
+        System.out.println("Enter the number of people");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println("Enter the number of apple");
+        int k = scan.nextInt();
+
+        if(n>k) {
+            System.out.println("Error");
+        }
+
+        else {
+            result = k % n;
+            System.out.println("Result: " + result);
+        }
+    }
+}
